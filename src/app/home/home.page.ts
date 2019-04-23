446293ed13bdc9f8584b825f7a7bbf50aaa89963
Tarek Jellali <jellalitarek@gmail.com>
2019-04-23 21:11:23 +0200

    Added few elements for testing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public weightList: Array<number>;
+  public dateList: Array<string>;
+  public inputWeight: number;
+  public inputDate = new Date(Date.now()).toISOString();
+  
+  constructor() {}
 
+  public ngOnInit() {
+    
+  }
 }
--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -35,6 +35,20 @@ module.exports = [
         "clobbers": [
             "Ionic.WebView"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -44,7 +58,8 @@ module.exports.metadata =
     "cordova-plugin-statusbar": "2.4.2",
     "cordova-plugin-splashscreen": "5.0.2",
     "cordova-plugin-ionic-webview": "3.1.2",
-    "cordova-plugin-ionic-keyboard": "2.1.3"
+    "cordova-plugin-ionic-keyboard": "2.1.3",
+    "cordova-plugin-device": "2.0.2"
 }
 // BOTTOM OF METADATA
 });
